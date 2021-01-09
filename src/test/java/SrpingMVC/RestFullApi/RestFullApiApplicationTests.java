eed7d036f0318e1a76e466a26e43323c7054c40d
ngdthinh97 <ngdthinh.97@gmail.com>
2021-01-09 11:25:07 +0700

    Init project for restfullAPI mvc webservice

---
+++ src/test/java/SrpingMVC/RestFullApi/RestFullApiApplicationTests.java
@@ -0,0 +1,13 @@
+package SrpingMVC.RestFullApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFullApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
